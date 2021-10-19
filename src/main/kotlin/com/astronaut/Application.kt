0733678e278dd54c7fbd5f1c71e2a176d3710f67
孤独な 宇宙飛行士 <vadim1230981@gmail.com>
2021-10-20 01:17:06 +0300

    basic Ktor blocking socket connection

---
+++ src/main/kotlin/com/astronaut/Application.kt
@@ -0,0 +1,38 @@
+package com.astronaut
+
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import kotlinx.coroutines.runBlocking
+import java.net.InetSocketAddress
+
+fun main() {
+    runBlocking {
+        val server = aSocket(ActorSelectorManager(this.coroutineContext)).tcp().bind(InetSocketAddress("0.0.0.0", 2323))
+        println("Started echo telnet server at ${server.localAddress}")
+
+        while (true) {
+            println("HERE")
+            val socket = server.accept()
+
+            println("Socket accepted: ${socket.remoteAddress}")
+
+            val input = socket.openReadChannel()
+            val output = socket.openWriteChannel(autoFlush = true)
+
+            try {
+                while (true) {
+                    val line = input.readUTF8Line(Int.MAX_VALUE)
+
+                    println("${socket.remoteAddress}: $line")
+
+                    val data = "$line\r\n"
+
+                    output.writeAvailable(data.encodeToByteArray(), 0, data.length)
+                }
+            } catch (e: Throwable) {
+                e.printStackTrace()
+                socket.close()
+            }
+        }
+    }
+}
